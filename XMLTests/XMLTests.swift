fb92a5f3521d5a98b8601782e0cf1b81734a6181
Craig Grummitt <craiggrummitt@gmail.com>
2017-11-10 16:59:11 +0000

    Remove UITests, Added Unit tests. Modified example code in ViewController and in readme. Added title image for readme.

--- XMLTests/XMLTests.swift
+++ XMLTests/XMLTests.swift
@@ -10,27 +10,112 @@ import XCTest
 @testable import XML
 
 class XMLTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  var url:URL!
+  var xml:XML!
+  override func setUp() {
+    super.setUp()
+    guard let url = Bundle.main.url(forResource: "books", withExtension: "xml") else {
+      fatalError("No URL")
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    self.url = url
+    guard let xml = XML(contentsOf: url) else {
+      fatalError("XML Can't be converted to XML")
     }
+    self.xml = xml
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func createBookNode()->XMLNode {
+    //Create an XML Node
+    let bookNode = XMLNode(name:"book")
+    //Add child node with name and value
+    bookNode.addChild(name: "title", value: "Robinson Crusoe")
+    bookNode.addChild(name: "author", value: "Daniel Defoe")
+    //Add attributes to node
+    bookNode.attributes["isbn"] = "9789835004100"
+    return(bookNode)
+  }
+  //Test creating an XML Node, including child nodes and attributes
+  func testCreateNode() {
+    let bookNode = createBookNode()
+    XCTAssertEqual(bookNode.attributes["isbn"], "9789835004100")
+    
+    let titleNode = bookNode["title"]
+    XCTAssertNotNil(titleNode)
+    XCTAssertEqual(titleNode!.text, "Robinson Crusoe")
+    
+    let authorNode = bookNode["author"]
+    XCTAssertNotNil(authorNode)
+    XCTAssertEqual(authorNode!.text, "Daniel Defoe")
+  }
+  //Test creating XML from file check it is brought in correctly.
+  func testCreateXMLFromFile() {
+    XCTAssertEqual(xml[0].children.count, 2)
+    let titleNode1 = xml[0][0]["title"]
+    XCTAssertNotNil(titleNode1)
+    XCTAssertEqual(titleNode1!.text, "Great Expectation")
+    
+    let titleNode2 = xml[0][1]["title"]
+    XCTAssertNotNil(titleNode2)
+    XCTAssertEqual(titleNode2!.text, "Don Quixote")
+  }
+  //Test adding and removing nodes, check children count is correct
+  func testAddingRemovingCountingNodes() {
+    //xml should be: Great Expectation, Don Quixote
+    //Check count = 2
+    XCTAssertEqual(xml[0].children.count, 2)
+    xml[0].addChild(createBookNode())
+    
+    //now xml should be: Great Expectation, Don Quixote, Robinson Crusoe
+    //Check count = 3
+    XCTAssertEqual(xml[0].children.count, 3)
+    
+    //Remove Don Quixote
+    xml[0].removeChild(at: 1)
+    //now xml should be: Great Expectation, Robinson Crusoe
+    
+    //Check count = 2
+    XCTAssertEqual(xml[0].children.count, 2)
+    
+    //Check children nodes are correct
+    let titleNode1 = xml[0][0]["title"]
+    XCTAssertNotNil(titleNode1)
+    XCTAssertEqual(titleNode1!.text, "Great Expectation")
+    
+    let titleNode2 = xml[0][1]["title"]
+    XCTAssertNotNil(titleNode2)
+    XCTAssertEqual(titleNode2!.text, "Robinson Crusoe")
+  }
+  func testSetWithSubscript() {
+    //first check the title of the first XML node
+    let titleNode1 = xml[0][0]["title"]
+    XCTAssertNotNil(titleNode1)
+    XCTAssertEqual(titleNode1!.text, "Great Expectation")
+    
+    //change the title of the first XML node
+    xml[0][0]["title"]?.text = "Great Expectations"
+    
+    //now check the title of the first XML node again
+    let titleNode2 = xml[0][0]["title"]
+    XCTAssertNotNil(titleNode2)
+    XCTAssertEqual(titleNode2!.text, "Great Expectations")
+    
+  }
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-# Simple XML parsing and serializing for Swift.
-
+![SwiftXML: Simple XML parsing and serializing for Swift. ](SwiftXML.png)
 ## Import an XML document 
 Import an XML document simply, and the XML structure will be converted to `XMLNode` objects.
 
@@ -8,9 +7,16 @@ guard let url = Bundle.main.url(forResource: "books", withExtension: "xml") else
 guard let xml = XML(contentsOf: url) else { return }
 ```
 ## Use subscripts
-Use subscripts to get and set nodes and properties.
+Use subscripts to set nodes and attributes:
 ```
 xml[0][0]["title"]?.text = "Great Expectations"
+xml[0][0].attributes["isbn"] = "38"
+```
+Similarly, use subscripts to get nodes and properties.
+```
+guard let title = xml[0][0]["title"]?.text,
+let isbn = xml[0][0].attributes["isbn"] else {return}
+print("\(title) with isbn of \(isbn)")
 ```
 ## Create an XML node
 Instantiate a node with its name, or its name and value:
@@ -26,8 +32,19 @@ Or with another XML node:
 ```
 xml[0].addChild(bookNode)
 ```
+## Remove child node
+You can remove a child node with its index:
+```
+xml[0].removeChild(at: 1)
+```
 ## Serialize XML
-Regenerate XML structure from your data with the XMLNode's `description` property.
+Regenerate XML structure from your data with the XMLNode's `description` property:
+```
+//either:
+print(xml.description)
+//or simply:
+print(xml)
+```
 
 # Install SwiftXML
 Drag the XML.swift file to your project
